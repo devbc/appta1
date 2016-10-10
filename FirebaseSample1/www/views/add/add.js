f53cd797be44a31410616df9546dd53564aeede0
Shaiwal Sharma <shaiwalsharma@Shaiwals-Air.T-mobile.com>
2016-10-10 01:31:01 -0700

    add screen

--- FirebaseSample1/www/views/add/add.js
+++ FirebaseSample1/www/views/add/add.js
@@ -1,16 +1,17 @@
 ﻿'Use Strict';
-angular.module('App').controller('addController', function ($rootScope, $scope, $state, $cordovaOauth, $localStorage, $location, $http,
+angular.module('App').controller('addController', function($rootScope, $scope, $state, $cordovaOauth, $localStorage, $location, $http,
     $ionicPopup, $firebaseObject, Auth, FURL, Utils, $firebaseArray, $ionicSlideBoxDelegate, $cordovaCamera, $cordovaGeolocation) {
-   
+    /*angular.module('App').controller('addController', function($rootScope, $scope, $state, $cordovaOauth, $localStorage, $location, $http,
+        $ionicPopup, $firebaseObject, Auth, FURL, Utils, $firebaseArray, $ionicSlideBoxDelegate) {*/
     jq('.photo_wrapper .photo_slide').css('display', 'block');
-   /* jq('.photo_wrapper').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        fade: true
-        //   asNavFor: '.photo_thumbs'
-    });*/
+    /* jq('.photo_wrapper').slick({
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         arrows: false,
+         dots: true,
+         fade: true
+         //   asNavFor: '.photo_thumbs'
+     });*/
 
     $scope.item = {};
     $scope.imageArr = [];
@@ -23,12 +24,12 @@ angular.module('App').controller('addController', function ($rootScope, $scope, 
     $scope.locDetails = null;
     $scope.locOptions = '';
 
-    $scope.slideHasChanged = function () {
+    $scope.slideHasChanged = function() {
         $ionicSlideBoxDelegate.update();
     };
 
     // Getting the image
-    $scope.getImage = function () {
+    $scope.getImage = function() {
         alert("inside getimage");
         if ($scope.imageArr.length < 4) {
             navigator.camera.getPicture(onSuccess, onFail, {
@@ -40,103 +41,103 @@ angular.module('App').controller('addController', function ($rootScope, $scope, 
         } else {
             alert("You have already added 4 images!");
         }
-          
-                
+
+
     }
-            
-    $scope.getCamera = function () {
-       // alert("inside getimage 1");
-        if ($scope.imageArr.length < 4){
-        //    alert("inside getimage 2");
+
+    $scope.getCamera = function() {
+        // alert("inside getimage 1");
+        if ($scope.imageArr.length < 4) {
+            //    alert("inside getimage 2");
             navigator.camera.getPicture(onSuccess, onFail, {
-                quality : 75, 
-                destinationType : Camera.DestinationType.DATA_URL, 
-                sourceType : Camera.PictureSourceType.CAMERA, 
-                allowEdit : false,
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: false,
                 encodingType: Camera.EncodingType.JPEG,
                 targetWidth: 480,
                 targetHeight: 640,
                 popoverOptions: CameraPopoverOptions,
                 saveToPhotoAlbum: false
-            
+
             });
-      
+
         } else {
             alert("You have already added 4 images!");
         }
     }
-            
-  //  $scope.getCamera();
+
+    //  $scope.getCamera();
 
     function onSuccess(imageData) {
-       
+
         $scope.imageArr.push("data:image/jpeg;base64," + imageData);
         $ionicSlideBoxDelegate.update();
         $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
-       alert("inside getimage 4>> " + $scope.imageArr.length);
-       
-  
-    } 
-            
+        alert("inside getimage 4>> " + $scope.imageArr.length);
+
+
+    }
+
     function onFail(message) {
         alert('Failed because: ' + message);
     }
-            
+
     // Getting Location 
- //   $scope.getLocation = function () {
-        var onGeoSuccess = function (position) {
-            var latlng = position.coords.latitude + ', ' + position.coords.longitude;
-            alert(latlng);
-            var geocoder = new google.maps.Geocoder();
-            var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            var request = {
-                latLng: latLng
-            };
-            $scope.item.lat = position.coords.latitude;
-            $scope.item.lng = position.coords.longitude;
-            geocoder.geocode(request, function (data, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    if (data[0] != null) {
-                        alert("address is: " + data[0].formatted_address);
-                        document.getElementById('userLoc').value = data[0].formatted_address;
-                        $scope.item.location = data[0].formatted_address;
-                    } else {
-                        alert("No address available");
-                    }
-                }
-            })
-            // Get location (city name, state)
-       
+    //   $scope.getLocation = function () {
+    var onGeoSuccess = function(position) {
+        var latlng = position.coords.latitude + ', ' + position.coords.longitude;
+        alert(latlng);
+        var geocoder = new google.maps.Geocoder();
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        var request = {
+            latLng: latLng
         };
-        // onError Callback receives a PositionError object
-        //
-        function onGeoError(error) {
-            alert(error.message);
-        }
-        navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
-  //  }
+        $scope.item.lat = position.coords.latitude;
+        $scope.item.lng = position.coords.longitude;
+        geocoder.geocode(request, function(data, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (data[0] != null) {
+                    alert("address is: " + data[0].formatted_address);
+                    document.getElementById('userLoc').value = data[0].formatted_address;
+                    $scope.item.location = data[0].formatted_address;
+                } else {
+                    alert("No address available");
+                }
+            }
+        });
+        // Get location (city name, state)
+
+    };
+    // onError Callback receives a PositionError object
+    //
+    function onGeoError(error) {
+        alert(error.message);
+    }
+    navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+    //  }
 
 
-    $scope.send = function () {
+    $scope.send = function() {
 
         for (var i = 0; i < $scope.imageArr.length; i++) {
             if (i == 0) {
                 $scope.parseFile = $scope.imageArr[i];
-            }else if(i ==1){
+            } else if (i == 1) {
                 $scope.parseFile2 = $scope.imageArr[i];
             } else if (i == 2) {
                 $scope.parseFile3 = $scope.imageArr[i];
             } else if (i == 3) {
                 $scope.parseFile4 = $scope.imageArr[i];
             }
-                
+
         }
-                
+
         // Checking if user is logged in      
 
         if ($rootScope.currentUser) {
             var itemJSON = {};
-            
+
             itemJSON = {
                 "itemName": $scope.item.itemName,
                 "approved": true,
@@ -144,7 +145,7 @@ angular.module('App').controller('addController', function ($rootScope, $scope, 
                 "itemDescription": null,
                 "itemPrice": $scope.item.itemPrice,
                 "reported": false,
-                "location":{
+                "location": {
                     "lat": $scope.item.lat,
                     "lon": $scope.item.lng,
                 },
@@ -160,16 +161,16 @@ angular.module('App').controller('addController', function ($rootScope, $scope, 
                 "owner": $rootScope.currentUser.email,
                 "created_on": new Date().getTime(),
                 "updated_on": new Date().getTime(),
-                "sold":"N"
+                "sold": "N"
             }
 
             var messagesRef = $firebaseArray(firebase.database().ref().child("items"));
-         
-           
-       //     messageListRef.push(itemJSON);
-           // alert(messagesRef);
-            messagesRef.$loaded().then(function (res) {
-                messagesRef.$add(itemJSON).then(function (ref) {
+
+
+            //     messageListRef.push(itemJSON);
+            // alert(messagesRef);
+            messagesRef.$loaded().then(function(res) {
+                messagesRef.$add(itemJSON).then(function(ref) {
                     //  list.$indexFor(id); // returns location in the array
                     $ionicPopup.alert({
                         title: 'Congratulations!',
@@ -177,13 +178,13 @@ angular.module('App').controller('addController', function ($rootScope, $scope, 
                     });
                     $state.transitionTo("app.home");
                 });
-               // alert(res);
+                // alert(res);
             });
-           
-        } 
+
+        }
     }
 
-    $scope.closeAddNew = function () {
+    $scope.closeAddNew = function() {
         $state.transitionTo("app.home");
     }
 
